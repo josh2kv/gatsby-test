fad8bdce9f7e6f9c1b6eeeaf9acf911806af754b
josh2kv <josh2kv@gmail.com>
2022-06-16 09:54:30 +0900

    render post's data

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
     siteMetadata: {
         title: `GatsbyJS Test`,
     },
-    plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+    plugins: [
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "blog",
+                path: `${__dirname}/blog`,
+            },
+        },
+        "gatsby-plugin-mdx",
+    ],
 };
